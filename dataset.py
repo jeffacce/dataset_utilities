e9e9c276f1b7a791ce11f3613ec6c8c98a22209f
Jeff Cui <cuizichen@gmail.com>
2020-07-22 14:40:34 +0800

    initial commit

---
+++ dataset.py
@@ -0,0 +1,292 @@
+import pandas as pd
+import numpy as np
+import yaml
+from tqdm import tqdm
+import datetime
+import warnings
+import pyodbc
+import os
+import importlib
+import uuid
+import subprocess
+
+
+# vectorized and adapted from:
+# https://stackoverflow.com/questions/3018758/determine-precision-and-scale-of-particular-number-in-python
+def magnitude_and_scale(x):
+    MAX_DIGITS = 15 # originally 14; change back to 14 if buggy
+    magnitude = x.copy()
+    scale = x.copy()
+    magnitude[:] = np.nan
+    scale[:] = np.nan
+    
+    x_abs = x.dropna().abs()
+    int_part = x_abs.astype(int)
+    magnitude_nona = np.log10(int_part.replace(0, 1)).astype(int) + 1
+    magnitude_nona = magnitude_nona.clip(1, MAX_DIGITS)
+    
+    frac_part = x_abs - int_part
+    multiplier = 10 ** (MAX_DIGITS - magnitude_nona)
+    frac_digits = (multiplier + (multiplier * frac_part + 0.5).astype(int)).astype(str)
+    frac_digits = frac_digits.str.rstrip('0').astype(float).astype(int)
+    scale.loc[~x.isna()] = np.log10(frac_digits).astype(int)
+    
+    magnitude.loc[~x.isna()] = magnitude_nona
+    
+    return magnitude, scale
+
+
+def get_type(x, force_allow_null=False):
+    has_null = (x.isna().sum() > 0) or force_allow_null
+    comment = ''
+    x = x.dropna().reset_index(drop=True)
+
+    MAX_PRECISION = 38
+    
+    if len(x) == 0:
+        result = 'nvarchar(255)'
+        comment = 'empty column, defaulting to nvarchar(255)'
+    else:
+        if pd.api.types.is_object_dtype(x):
+            if type(x.iloc[0]) is datetime.date:
+                result = 'date'
+            elif type(x.iloc[0]) is datetime.datetime or type(x.iloc[0]) is pd.Timestamp:
+                result = 'datetime'
+            else:
+                size = min(int(x.astype(str).str.len().max() * 2), 4000)
+                result = 'nvarchar(%s)' % size
+        elif pd.api.types.is_numeric_dtype(x):
+            magnitude, scale = magnitude_and_scale(x)
+            magnitude = magnitude.max()
+            scale = scale.max()
+            
+            if (scale == 0) or pd.api.types.is_integer_dtype(x):
+                if ((x >= 0) & (x <= 1)).all():
+                    result = 'bit'
+                elif ((x >= 0) & (x <= 2**8-1)).all():
+                    result = 'tinyint'
+                elif ((x >= -2**15) & (x <= 2**15-1)).all():
+                    result = 'smallint'
+                elif ((x >= -2**31) & (x <= 2**31-1)).all():
+                    result = 'int'
+                else:
+                    result = 'bigint'
+            else:
+                precision = magnitude + scale
+                if precision > MAX_PRECISION:
+                    result = 'nvarchar(255)'
+                    comment = 'number too big for decimal, falling back to nvarchar(255)'
+                else:
+                    ratio = min(MAX_PRECISION / precision, 1.5)
+                    magnitude = int(magnitude * ratio)
+                    scale = int(scale * ratio)
+                    precision = magnitude + scale
+                    result = 'decimal(%s, %s)' % (precision, scale)
+        elif pd.api.types.is_datetime64_any_dtype(x):
+            result = 'datetime'
+        else:
+            result = 'nvarchar(255)'
+            comment = 'unable to infer type, defaulting to nvarchar(255);'
+    
+    if has_null:
+        result += ' NULL'
+    
+    return result, comment
+
+
+def indent(str_arr, times=1, spaces=4):
+    indentation = ' ' * spaces
+    return [indentation + x for x in str_arr]
+
+
+def get_create_statement(df, table_name, force_allow_null=False, sample=None, verbose=False):
+    template = '''CREATE TABLE %s (\n%s\n) ON [PRIMARY];'''
+    
+    if not sample is None:
+        df = df.sample(sample)
+    
+    col_defs = []
+    cols = df.columns
+    
+    if verbose:
+        cols = tqdm(cols)
+
+    for col in cols: 
+        vartype, comment = get_type(df[col], force_allow_null=force_allow_null)
+        if comment != '':
+            col_defs.append('-- %s' % comment)
+        col_defs.append('[%s] %s' % (col, vartype))
+    col_defs = ',\n'.join(indent(col_defs))
+    
+    return template % (table_name, col_defs)
+
+
+def _try_import(s):
+    arr = s.lstrip('.').split('.')
+
+    if len(arr) == 1:
+        module_name = arr[0]
+    elif len(arr) == 2:
+        module_name = arr[0]
+        method_name = arr[1]
+    elif len(arr) >= 3:
+        package_name = arr[0]
+        module_name = '.' + '.'.join(arr[1:-1])
+        method_name = arr[-1]
+    
+    if package_name is None:
+        module = importlib.import_module(module_name)
+    else:
+        module = importlib.import_module(module_name, package=package_name)
+    if method_name is None:
+        return module
+    else:
+        return getattr(module, method_name)
+
+
+class dataset:
+    def __init__(self, config_path=None):
+        if config_path is None:
+            self.config = {}
+        else:
+            with open(config_path, 'r', encoding='utf-8-sig') as f:
+                self.config = yaml.safe_load(f)
+        
+        if 'transform' in self.config:
+            self.transform_function = _try_import(self.config['transform'])
+        else:
+            self.transform_function = None
+    
+    def read(self, filepath=None):
+        if filepath is None:
+            if 'filepath' in self.config:
+                filepath = self.config['filepath']
+            else:
+                raise ValueError('filepath must be specified here or in the config file.')
+        
+        ext = os.path.splitext(filepath)[1]
+        if ext == '.csv':
+            self.data = pd.read_csv(
+                self.config['filepath'],
+                encoding=self.config.get('encoding'),
+                float_precision='round_trip',
+            )
+        elif ext in ['.h5', '.hdf5', '.hdf']:
+            self.data = pd.read_hdf(self.config['filepath'])
+        elif ext in ['.xls', '.xlsx']:
+            self.data = pd.read_excel(
+                self.config['filepath'],
+                header=self.config.get('header', 0),
+                sheet_name=self.config.get('sheet_name', 0),
+                encoding=self.config.get('encoding'),
+            )
+        else:
+            raise ValueError('Only .h5/hdf/hdf5/csv/xls/xlsx supported.')
+        return self
+    
+    def write(self, filepath=None):
+        if filepath is None:
+            if 'filepath' in self.config:
+                filepath = self.config['filepath']
+            else:
+                raise ValueError('filepath must be specified here or in the config file.')
+        ext = os.path.splitext(filepath)[1]
+        if ext == '.csv':
+            self.data.to_csv(filepath, index=False, encoding='utf-8-sig')
+        elif ext in ['.h5', '.hdf5', '.hdf']:
+            self.data.to_hdf(filepath, index=False, encoding='utf-8-sig')
+        elif ext in ['.xls', 'xlsx']:
+            self.data.to_excel(filepath, index=False, encoding='utf-8-sig')
+        else:
+            raise ValueError('Only .h5/hdf/hdf5/csv/xls/xlsx supported.')
+    
+    def transform(self, transform_function=None):
+        if not transform_function is None:
+            self.data = transform_function(self.data)
+        else:
+            if not self.transform_function is None:
+                self.data = self.transform_function(self.data)
+        return self
+
+
+class sql_dataset(dataset):
+    def query(self, chunksize=100, pbar=True):
+        conn = pyodbc.connect(**self.config['conn'])
+        if 'get_row_count' in self.config['query']:
+            row_count = pd.read_sql(self.config['query']['get_row_count'], conn).loc[0].values[0]
+            chunk_count = np.ceil(row_count / chunksize).astype(int)
+        else:
+            if pbar:
+                warnings.warn('Progress bar is disabled when get_row_count query is not specified in the config file.')
+                pbar = False
+        
+        chunks = pd.read_sql(self.config['query']['get_data'], conn, chunksize=chunksize)
+        if pbar:
+            chunks = tqdm(chunks, total=chunk_count)
+
+        data = []
+        for chunk in chunks:
+            data.append(chunk)
+        self.data = pd.concat(data)
+        
+        return self
+    
+    def upload_bcp(self, mode='append', verbose=False, schema_sample=None):
+        host_config_args = [
+            '-S', self.config['conn']['server'],
+            '-d', self.config['conn']['database'],
+            '-U', self.config['conn']['user'],
+            '-P', self.config['conn']['password'],
+        ]
+        
+        if mode == 'append':
+            pass
+        elif mode == 'overwrite_data':
+            p = subprocess.Popen([
+                'sqlcmd',
+                *host_config_args,
+                '-Q', 'TRUNCATE TABLE dbo.%s;' % self.config['table'],
+            ]).wait()
+        elif mode == 'overwrite_table':
+            # drop old table
+            p = subprocess.Popen([
+                'sqlcmd',
+                *host_config_args,
+                '-Q', "IF OBJECT_ID('dbo.%s', 'U') IS NOT NULL DROP TABLE dbo.%s;" % (self.config['table'], self.config['table']),
+            ])
+            
+            # get schema definition
+            schema_def_query = get_create_statement(
+                self.data, self.config['table'],
+                force_allow_null=True,
+                verbose=verbose,
+                sample=schema_sample,
+            )
+            
+            # create new table
+            p = subprocess.Popen([
+                'sqlcmd',
+                *host_config_args,
+                '-Q', schema_def_query,
+            ])
+        else:
+            raise ValueError("mode must be one of ['append', 'overwrite_data', 'overwrite_table']")
+        
+        temp_filename = 'bcp_temp_%s' % uuid.uuid4()
+        self.data.to_csv(temp_filename + '.csv', sep='$', index=False)
+
+        p = subprocess.Popen([
+            'bcp',
+            self.config['table'],
+            'in',
+            temp_filename + '.csv',
+            '-c', r'-t\$', '-k', '-E',
+            '-e', temp_filename + '.err',
+            '-F2',
+            *host_config_args,
+        ]).wait()
+
+        # clean up temp csv
+        if os.path.exists(temp_filename + '.csv'):
+            os.remove(temp_filename + '.csv')
+        
