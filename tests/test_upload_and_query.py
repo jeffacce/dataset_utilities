45b8a063b1a9dffb9da21bd62a7eb8658dd776b9
Jeff Cui <cuizichen@gmail.com>
2020-11-02 17:06:30 +0800

    test upload overwrite table, ping

--- tests/test_upload_and_query.py
+++ tests/test_upload_and_query.py
@@ -2,28 +2,41 @@ import pytest
 from ..dataset import sql_dataset
 import pandas as pd
 import numpy as np
+import requests
 
 
-def test_read_upload_bcp_query(verbose=False):
+def test_read_upload_bcp_query_overwrite_table(verbose=False):
     sd = sql_dataset('./tests/database.yml').read()
     sd.data['dt'] = pd.to_datetime(sd.data['dt'])
     df_orig = sd.data.copy()
     sd.upload(mode='overwrite_table', bcp=True, verbose=verbose)
     df_queried = sd.query().data
-
     return (df_queried == df_orig).all()
 
+def test_read_upload_bcp_query_overwrite_data(verbose=False):
+    sd = sql_dataset('./tests/database.yml').read()
+    sd.data['dt'] = pd.to_datetime(sd.data['dt'])
+    df_orig = sd.data.copy()
+    sd.upload(mode='overwrite_data', bcp=True, verbose=verbose)
+    df_queried = sd.query().data
+    return (df_queried == df_orig).all()
 
-def test_read_upload_pyodbc_query(verbose=False):
+def test_read_upload_pyodbc_query_overwrite_table(verbose=False):
     sd = sql_dataset('./tests/database.yml').read()
     sd.data['dt'] = pd.to_datetime(sd.data['dt'])
     df_orig = sd.data.copy()
     sd.upload(mode='overwrite_table', bcp=False, verbose=verbose)
     df_queried = sd.query().data
-
     return (df_queried == df_orig).all()
 
+def test_read_upload_pyodbc_query_overwrite_data(verbose=False):
+    sd = sql_dataset('./tests/database.yml').read()
+    sd.data['dt'] = pd.to_datetime(sd.data['dt'])
+    df_orig = sd.data.copy()
+    sd.upload(mode='overwrite_data', bcp=False, verbose=verbose)
+    df_queried = sd.query().data
+    return (df_queried == df_orig).all()
 
-if __name__ == '__main__':
-    test_read_upload_pyodbc_query(True)
-    test_read_upload_bcp_query(True)
+def test_ping_fake_database_raises_connection_error():
+    sd = sql_dataset('./tests/fake_database.yml').read()
+    assert not sd.ping(max_retries=1)
