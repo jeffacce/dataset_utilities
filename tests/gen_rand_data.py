9072329b89243994301d0be19bef60668b081ed3
Jeff Cui <cuizichen@gmail.com>
2020-11-27 15:00:50 +0800

    test data add more cols, NAs

--- tests/gen_rand_data.py
+++ tests/gen_rand_data.py
@@ -5,8 +5,12 @@ def rand_str_array(n, str_len):
     result = pd.DataFrame(np.random.randint(97, 123, (n, str_len)))
     return result.applymap(chr).sum(axis=1)
 
-def rand_float_array(n):
-    return pd.Series(np.random.random(n))
+def rand_float_array(n, na_ratio=0):
+    result = pd.Series(np.random.random(n))
+    if na_ratio > 0:
+        na_idx = np.random.choice(range(n), size=int(min(na_ratio, 1) * n), replace=False)
+        result.loc[na_idx] = np.nan
+    return result
 
 def rand_dt_array(n):
     start = int(pd.Timestamp('2020-01-01 12:00:00 AM').value / 1e9)
@@ -19,8 +23,10 @@ def rand_df(n):
     # gen random data of datetime, string, and float
     return pd.DataFrame({
         'dt': rand_dt_array(n),
-        'id': rand_str_array(n, 50),
-        'float': rand_float_array(n),
+        'uid': rand_str_array(n, 50),
+        'float': rand_float_array(n, na_ratio=0.2),
+        'float2': rand_float_array(n, na_ratio=0),
+        'name': rand_str_array(n, 30),
     })
 
 df = rand_df(100000)
