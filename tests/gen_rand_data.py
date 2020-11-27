958d618a38405e1eecee314743b9589d9420a60a
Jeff Cui <cuizichen@gmail.com>
2020-11-27 15:27:53 +0800

    add empty series to integration test

--- tests/gen_rand_data.py
+++ tests/gen_rand_data.py
@@ -19,11 +19,15 @@ def rand_dt_array(n):
     result = pd.to_datetime(result, unit='s')
     return result
 
+def empty_series(n):
+    return pd.Series([np.nan] * n)
+
 def rand_df(n):
     # gen random data of datetime, string, and float
     return pd.DataFrame({
         'dt': rand_dt_array(n),
         'uid': rand_str_array(n, 50),
+        'empty_col': empty_series(n),
         'float': rand_float_array(n, na_ratio=0.2),
         'float2': rand_float_array(n, na_ratio=0),
         'name': rand_str_array(n, 30),
